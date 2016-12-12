c1de9ac6fe6c13d7dcafbff5050a7750a5409286
Pablo Hernández <devphernandez@gmail.com>
2016-12-12 18:03:39 -0300

    test mdl

--- application/view/header.php
+++ application/view/header.php
@@ -1,3 +1,12 @@
 <?php
 	include '../controller/handler.php';
 ?>
+<html>
+	<head>
+		<title>Optica</title>
+		<!--MDL-->
+		<link 	rel="stylesheet" href="../../resource/css/material.min.css" >
+		<link 	rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<script src="../../resource/js/material.min.js"></script>
+	</head>
+	<body>
