483beabed6aa4d87e40271b5d76593b8bdf516f6
Pablo Hernández <devphernandez@gmail.com>
2016-12-13 17:54:42 -0300

    template

--- application/view/header.php
+++ application/view/header.php
@@ -4,16 +4,22 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="UTF-8">
 		<title>Optica</title>
 		<!--Importar los iconos de google-->
 		<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<!--Importar hoja de estilos de la pagina-->
+		<link 	rel="stylesheet" href="../../resource/css/style.css">
 		<!--Importar materialize.min.css-->
 		<link 	rel="stylesheet" href="../../resource/css/materialize.min.css" media="screen,projection">
+		
 		<!--Permitir que el sitio web del navegador esté optimizado para móviles-->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		
 		<!--Importar jQuery antes que materialize.min.js-->
 		<script src="../../resource/js/jquery-2.2.4.min.js"></script>
 		<script src="../../resource/js/materialize.min.js"></script>
+		<script src="../../resource/js/script.js"></script>
 	</head>
 	<body>
+	
--- application/view/inventario.php
+++ application/view/inventario.php
@@ -1,1 +1,23 @@
 <?php include 'header.php'; ?>
+<?php include 'navBar.php'; ?>
+		<main>
+            <div class="container">
+				<div class="row">
+					<div class="col l5 m12 s12 z-depth-2 card-panel">
+						<form class="login-form">
+							<div class="row">
+								<div class="input-field">
+									<i class="mdi-social-person-outline prefix">account_circle</i>
+									<input placeholder="Placeholder" id="first_name" type="text" class="validate">
+									<label for="first_name">Nombre del Producto</label>
+								</div>
+							</div>
+						</form>
+					</div>
+					<div class="col l5 m12 s12 offset-l1 z-depth-2 card-panel">
+						Promo Content 2 goes here -->
+					</div>
+				</div>
+			</div>
+		</main>
+<?php include 'footer.php'; ?>
